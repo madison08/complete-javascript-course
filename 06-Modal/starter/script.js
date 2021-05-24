b92d64f274d5951a298cbc89bf6ae2a35e9da68d
madison <madisonamoa@gmail.com>
2021-05-24 20:00:46 +0000

    finish Modal

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,43 @@
 'use strict';
+
+
+const btnOpen = document.querySelectorAll('.show-modal')
+
+const modal = document.querySelector('.modal')
+
+
+const btnClose = document.querySelector('.close-modal')
+
+const overlay = document.querySelector('.overlay')
+
+const openModal = function(){
+    modal.classList.remove('hidden')
+    overlay.classList.remove('hidden')
+}
+
+for(let i = 0; i < btnOpen.length; i++ ){
+    console.log(btnOpen[i])
+
+    btnOpen[i].addEventListener('click',openModal)
+
+}
+
+const closeModal = function(){
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden')
+}
+
+btnClose.addEventListener('click',closeModal)
+
+overlay.addEventListener('click',closeModal)
+
+
+document.addEventListener('keydown', function(e){
+    console.log("was pressed")
+    if(e.key == "Escape"){
+        if(!modal.classList.contains('hidden')){
+            closeModal()
+
+        }
+    }
+})
